91578c03dbcc5f2f9be086a9cb29e2b5a025fead
Tykhon <richard.lee970112@gmail.com>
2022-07-24 08:29:56 +0900

    resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Particles from 'react-particles-js';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles((theme) => ({
+  Particles: {
+    position: 'fixed',
+    opacity: '0.3',
+  },
   timeLine: {
     position: 'relative',
     padding: '1rem',
@@ -175,6 +180,47 @@ const Resume = () => {
           {/* end */}
         </Box>
       </Box>
+      <Particles
+        canvasClassName={classes.Particles}
+        params={{
+          particles: {
+            number: {
+              value: 45,
+              density: {
+                enable: true,
+                value_area: 900,
+              },
+            },
+            shape: {
+              type: 'circle',
+              stroke: {
+                width: 1,
+                color: 'tomato',
+              },
+            },
+            size: {
+              value: 3,
+              random: true,
+              animation: {
+                enable: true,
+                speed: 10,
+                size_min: 0.1,
+                sync: true,
+              },
+            },
+            opacity: {
+              value: 1,
+              random: true,
+              animation: {
+                enable: true,
+                speed: 1,
+                opacity_min: 0.1,
+                sync: true,
+              },
+            },
+          },
+        }}
+      />
     </>
   );
 };
